7a787f2f2c8a8e44944ee019f9e3031dd1f00799
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2025-04-05 11:10:20 +0200

    [grvgm] change trig to unit circle arg

--- src/grvgm_math.c
+++ src/grvgm_math.c
@@ -1,11 +1,11 @@
 #include "grvgm.h"
 
 fx32 grvgm_sin(fx32 x) {
-    double phi = fx32_to_f64(x);
+    double phi = fx32_to_f64(x) * 2.0 * M_PI;
     return fx32_from_f64(sin(phi));
 }
 
 fx32 grvgm_cos(fx32 x) {
-    double phi = fx32_to_f64(x);
+    double phi = fx32_to_f64(x) * 2.0 * M_PI;
     return fx32_from_f64(cos(phi));
 }
