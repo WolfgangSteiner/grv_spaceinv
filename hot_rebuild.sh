c8ab912455680f15eaee2176006670157bf0c400
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2025-04-14 08:46:45 +0200

    fix hot_rebuild script

--- hot_rebuild.sh
+++ hot_rebuild.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
 gcc -fPIC -shared -o build/libspaceinv.so -I. -Iinclude -Ilib/grv/include src/spaceinv.c
-gcc -fPIC -shared -o build/libsynth.so -I. -Iinclude -Ilib/grv/include src/synth.c
+gcc -fPIC -shared -o build/libsynth.so -I. -Iinclude -Ilib/grv/include src/synth.c src/synth_audio.c src/synth_gui.c
