299da2bce4b0b810d1fd636ac339223508784fd2
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2025-04-14 08:46:45 +0200

    update environment

--- flake.nix
+++ flake.nix
@@ -1,34 +1,36 @@
 {
-  description = "SDL2 development environment";
+	description = "SDL2 development environment";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+	};
 
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux"; # Change if using another architecture
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.SDL2
-          pkgs.SDL2_image
-          pkgs.SDL2_mixer
-          pkgs.SDL2_ttf
-          pkgs.SDL2_net
-          pkgs.gcc
-          pkgs.clang
-          pkgs.tinycc
-          pkgs.ccache
-          pkgs.zstd
-        ];
+	outputs = { nixpkgs, ... }:
+		let
+			system = "x86_64-linux"; # Change if using another architecture
+			pkgs = import nixpkgs { inherit system; };
+		in {
+			devShells.${system}.default = pkgs.mkShell {
+				buildInputs = [
+					pkgs.SDL2
+					pkgs.SDL2_image
+					pkgs.SDL2_mixer
+					pkgs.SDL2_ttf
+					pkgs.SDL2_net
+					pkgs.gcc
+					pkgs.clang
+					pkgs.tinycc
+					pkgs.ccache
+					pkgs.zstd
+					pkgs.bear
+					pkgs.ccls
+				];
 
-        shellHook = ''
-          echo "SDL2 development environment activated!"
-          export LD_LIBRARY_PATH=build:lib/grv/build:$LD_LIBRARY_PATH
-		  export EDITOR=nvim
-        '';
-      };
-    };
+				shellHook = ''
+					echo "SDL2 development environment activated!"
+					export LD_LIBRARY_PATH=build:lib/grv/build:$LD_LIBRARY_PATH
+					export EDITOR=nvim
+				'';
+			};
+		};
 }
