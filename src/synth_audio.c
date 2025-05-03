04d965d0f14f397a2da4ec9f61421c861065157b
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2025-05-03 10:01:36 +0200

    [synth] implement saw and rectangle waves

--- src/synth_audio.c
+++ src/synth_audio.c
@@ -237,12 +237,21 @@ f32* process_envelope(f32 gate, bool trigger_received, envelope_t* env, grv_aren
 	return outptr;
 }
 
-f32* fill_phase_buffer(grv_arena_t* arena, f32* freq, f32* phase_state) {
+f32* fill_phase_diff_buffer(f32* freq, grv_arena_t* arena) {
+	f32* outptr = arena_alloc_buffer(arena);
+	f32* dst = outptr;
+	for (i32 i = 0; i < AUDIO_FRAME_SIZE; i++) {
+		*dst++ = (*freq++) / GRVGM_SAMPLE_RATE;
+	}
+	return outptr;
+}
+
+f32* fill_phase_buffer(f32* phase_diff, f32* phase_state, grv_arena_t* arena) {
 	f32* outptr = arena_alloc_buffer(arena);
 	f32* dst = outptr;
 	f32 ph = *phase_state;
 	for (i32 i = 0; i < AUDIO_FRAME_SIZE; i++) {
-		ph += (*freq++) / GRVGM_SAMPLE_RATE;
+		ph += (*phase_diff++);
 		if (ph >= 1.0f) ph -= 1.0f;
 		*dst++ = ph;
 	}
@@ -264,6 +273,58 @@ f32* oscillatr_render_noise(grv_arena_t* arena) {
 	f32* dst = outptr;
 	for (i32 i = 0; i < AUDIO_FRAME_SIZE; i++) {
 		*dst++ = -1.0f + 2.0f * grvgm_random_f32();
+	}
+	return outptr;
+}
+
+f32 poly_blep(f32 t, f32 dt) {
+    if (t < dt) {
+        t /= dt;
+        return 2*t - t*t - 1.0f;
+    }
+    else if (t > 1.0f - dt) {
+        t = (t - 1.0f) / dt;
+        return t*t + 2*t + 1.0f;
+    }
+    else return 0.0f;
+}
+
+f32 poly_blep_rect(f32 t, f32 dt) {
+    if (t < dt) {
+        t /= dt;
+        return 2*t - t*t - 1.0f;
+    } else if (t - 0.5f >= 0.0f && t - 0.5f < dt) {
+		t = (t - 0.5f) / dt;
+		return -(2*t - t*t - 1.0f);
+	} else if (0.5f - t >= 0 && 0.5f - t < dt) {
+		t = (t - 0.5f) / dt;
+		return -(t*t + 2*t + 1.0f);
+	} else if (t > 1.0f - dt) {
+        t = (t - 1.0f) / dt;
+        return t*t + 2*t + 1.0f;
+    }
+    else return 0.0f;
+}
+
+f32* oscillator_render_rect(f32* phase, f32* phase_diff, grv_arena_t* arena) {
+	f32* outptr = arena_alloc_buffer(arena);
+	f32* dst = outptr;
+	for (i32 i = 0; i < AUDIO_FRAME_SIZE; i++) {
+		f32 phi = (*phase++);
+		f32 y = phi <= 0.5f ? 1.0f : -1.0f;
+		y += poly_blep_rect(phi, *phase_diff++);
+		*dst++ = y;
+	}
+	return outptr;
+}
+
+f32* oscillator_render_saw(f32* phase, f32* phase_diff, grv_arena_t* arena) {
+	f32* outptr = arena_alloc_buffer(arena);
+	f32* dst = outptr;
+	for (i32 i = 0; i < AUDIO_FRAME_SIZE; i++) {
+		f32 y = 2.0f * (*phase) - 1.0f;
+		y -= poly_blep(*phase++, *phase_diff++);
+		*dst++ = y;
 	}
 	return outptr;
 }
@@ -420,12 +481,20 @@ f32* process_test_tone(grv_arena_t* arena, f32* phase) {
 	return outptr;
 }
 
-f32* process_oscillator(oscillator_t* osc, f32* phase_buffer, grv_arena_t* arena) {
+f32* process_oscillator(
+	oscillator_t* osc,
+	f32* phase,
+	f32* phase_diff,
+	grv_arena_t* arena) {
 	switch (osc->wave_type) {
+	case WAVE_TYPE_RECT:
+		return oscillator_render_rect(phase, phase_diff, arena);
+	case WAVE_TYPE_SAW:
+		return oscillator_render_saw(phase, phase_diff, arena);
 	case WAVE_TYPE_NOISE:
 		return oscillatr_render_noise(arena);
 	default:
-		return sine_osc(arena, phase_buffer);
+		return sine_osc(arena, phase);
 	}
 }
 
@@ -442,8 +511,9 @@ void process_simple_synth(
 		&synth->note_proc.smoothed_freq,
 		0.01f,
 		arena);
-	f32* phase = fill_phase_buffer(arena, freq, &synth->phase_state);
-	f32* mono_buffer = process_oscillator(&synth->oscillator, phase, arena);
+	f32* phase_diff = fill_phase_diff_buffer(freq, arena);
+	f32* phase = fill_phase_buffer(phase_diff, &synth->phase_state, arena);
+	f32* mono_buffer = process_oscillator(&synth->oscillator, phase, phase_diff, arena);
 	f32* amp_env = process_envelope(
 		synth->note_proc.gate,
 		synth->note_proc.trigger_received,
